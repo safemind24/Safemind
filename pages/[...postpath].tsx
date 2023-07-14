16bf46b894231ce267e4f6dba9e570617beb1d20
safemind24 <139577073+safemind24@users.noreply.github.com>
2023-07-15 03:59:13 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://www.jobsowner.com"
+	const endpoint = "https://alcashzone.com/"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://jobsowner.com/` + encodeURI(path as string)
+					`https://alcashzone.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
